1219fa4f6fdfd81737c1d2a9c5f7bdbc2f989167
austinfulks <54602734+austinfulks@users.noreply.github.com>
2020-01-23 13:45:19 -0800

    Add files via upload

---
+++ JUICEWRLDMELDY.rb
@@ -0,0 +1,18 @@
+# Welcome to Sonic Pi v3.1
+use_bpm 80
+live_loop :AYUP_up do
+  play:B5
+  play:D5
+  sleep 1
+  play:D5
+  play:B4
+  play:D4
+  sleep 0.5
+  play:Fs5
+  sleep 1
+  play:E5
+  sleep 0.5
+  play:Cs5
+  sleep 1
+  play:D5
+end
